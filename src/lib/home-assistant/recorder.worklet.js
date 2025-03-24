23dccc13c8fa883e153245b4f51ab32539766049
Aidan Timson <aidan@timmo.dev>
2025-03-24 15:43:53 +0000

    Ignore

--- src/lib/home-assistant/recorder.worklet.js
+++ src/lib/home-assistant/recorder.worklet.js
@@ -1,4 +1,6 @@
+// @ts-ignore
 class RecorderProcessor extends AudioWorkletProcessor {
+  // @ts-ignore
   process(inputList, _outputList, _parameters) {
     if (inputList[0].length < 1) {
       return true;
@@ -12,10 +14,12 @@ class RecorderProcessor extends AudioWorkletProcessor {
       int16Data[i] = s < 0 ? s * 0x8000 : s * 0x7fff;
     }
 
+    // @ts-ignore
     this.port.postMessage(int16Data);
 
     return true;
   }
 }
 
+// @ts-ignore
 registerProcessor("recorder.worklet", RecorderProcessor);
